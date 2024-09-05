90341a9b16fb8aea61f07f579e73f05d45a288be
IAstanto7 <astantoismail@gmail.com>
2024-09-05 09:02:07 +0700

    continue code "set form"

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +17,45 @@ const Contact = () => {
   })
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {}
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    
+    emailjs.send(
+      'service_v67m9dq',
+      'template_ntkz9bi',
+      {
+        from_name: form.name,
+        to_name: 'Ismail',
+        from_email: form.email,
+        to_email: 'iastanto7@gmail.com',
+        message: form.message,
+      },
+      'VjJ-LWzR4vC1XXYzv'
+    )
+    .then(() =>  {
+      setLoading(false);
+      alert('Thank you, I will get back to you as soon as possible.');
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }, (error) => {
+      setLoading(false)
+
+      console.log(error);
+
+      alert('Something went wrong.')
+    })
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
@@ -62,8 +99,7 @@ const Contact = () => {
             <span className="text-white font-medium mb-4">Your Message</span>
             <textarea
               rows="7" 
-              type="text" 
-              name="name"
+              name="message"
               value={form.message}
               onChange={handleChange}
               placeholder="What do you want to say?"
